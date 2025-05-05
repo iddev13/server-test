743016e55279622f8429064506c534ed9a51d841
Igor <80629173+iddev13@users.noreply.github.com>
2025-05-05 20:59:41 +0300

    1.0.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["cobalt.kh.ua", "localhost:3000"],
+    },
+  },
 };
 
 export default nextConfig;
