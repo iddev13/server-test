ee57f8773e82866606a4e9f5b4e35353f2a9c01b
Igor <80629173+iddev13@users.noreply.github.com>
2025-05-08 17:06:50 +0300

    1.0.1

---
+++ src/actions/get-data.ts
@@ -0,0 +1,5 @@
+'use server';
+
+export async function getData() {
+	return ['First', 'Second', 'Third'];
+}
---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+const AboutPage = () => {
+	return (
+		<div>
+			<h1>About Page</h1>
+			<p>client page</p>
+		</div>
+	);
+};
+
+export default AboutPage;
